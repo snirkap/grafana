c1b4f350058055950957a9b6b3c04a112b97529f
snir kapah <120733215+snirkap@users.noreply.github.com>
2023-12-01 19:11:47 +0200

    Create setup-grafana.sh

---
+++ setup-grafana.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Stop execution if any command fails
+set -e
+
+# Create a Kubernetes cluster using kind
+kind create cluster --name monitoring --image kindest/node:v1.23.6 --config kind.yaml
+
+# Create Kubernetes objects defined in the Grafana manifests
+kubectl create -f manifests/setup/
+kubectl create -f manifests/
+
+# Sleep for 2.5 minutes to allow pods to start up
+echo "Waiting for 2:30 minutes to allow pods to start..."
+sleep 150
+
+# Port forward Grafana service to be accessible externally
+kubectl -n monitoring port-forward svc/grafana 3000 --address='0.0.0.0' &
+
+
+
+
